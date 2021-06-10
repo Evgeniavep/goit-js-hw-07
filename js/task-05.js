f0e836561463b5626ecb3c15a80071d071ea4c66
Evhenia Vepretskaya <enychka12@gmail.com>
2021-06-10 13:38:07 +0300

    Update-task-05

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,5 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input",
-    (event) => (output.textContent =
-        input.value === "" ? "незнакомец" : event.currentTarget.value)
+    (event) => (output.textContent = event.currentTarget.value.trim() || "незнакомец")
 );
